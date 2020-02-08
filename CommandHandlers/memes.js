05f53e9e0a03690376e50bda8797d6466090a145
Ahmed Hanie <zwya@ymail.com>
2020-02-08 18:09:55 +0200

    Fix timeouts for waiting between sending memes

--- CommandHandlers/memes.js
+++ CommandHandlers/memes.js
@@ -4,7 +4,6 @@ const util = require('../util/util.js');
 
 var memes = {};
 var channel = false;
-var timeoutSet = false;
 
 module.exports.init = (client) => {
   memes['data'] = {};
@@ -18,6 +17,12 @@ module.exports.init = (client) => {
     channel = client.channels.get(meme['channel']);
     if (channel) {
       memes['channel'] = meme['channel'];
+    }
+  }
+  else if ('defaultChannel' in meme) {
+    channel = client.channels.get(meme['defaultChannel']);
+    if (channel) {
+      memes['channel'] = meme['defaultChannel'];
     }
   }
 
@@ -38,41 +43,8 @@ module.exports.setChannel = (client, channelid) => {
   if (newChannel && newChannel.type == 'text') {
     memes['channel'] = channelid;
     channel = newChannel;
-    handleMeme();
+    postMemeFetch();
   }
-}
-
-function handleMeme() {
-  const keys = Object.keys(memes.data);
-  for (var i=0;i<keys.length;i++) {
-    const dt = new Date(memes['data'][keys[i]]['created_utc'] * 1000);
-    if (new Date() - dt / 1000 / 60 / 60 >= 72) {
-      delete memes[keys[i]];
-    }
-  }
-  var condition = false;
-  if ('lastpost' in memes) {
-    const lastPostDate = new Date(memes['lastpost']);
-    condition = (channel && (new Date() - lastPostDate) / 1000 / 60 / 60 > 6);
-  }
-  else {
-    condition = (channel);
-  }
-  if (condition) {
-    var key = false;
-    for (var i=0;i<keys.length;i++) {
-      if (!memes['data'][keys[i]]['posted']) {
-        key = keys[i];
-        break;
-      }
-    }
-    if (key) {
-      channel.send(memes['data'][keys[i]]['title'] + '\n' + memes['data'][keys[i]]['url']);
-      memes['data'][keys[i]]['posted'] = true;
-      memes['lastpost'] = new Date();
-    }
-  }
-  util.savejson('memes.json', memes);
 }
 
 function getMemes(callback) {
@@ -90,14 +62,31 @@ function getMemes(callback) {
 }
 
 function postMemeFetch() {
-  handleMeme();
-  if (countNotPosted() < 10) {
-    setTimeout(() => {
-      getMemes(postMemeFetch);
-    }, 1000 * 60 * 60 * 6);
-  }
-  else {
-    setTimeout(postMemeFetch, 1000 * 60 * 60 * 6);
+  deleteOldMemes();
+  sendMeme();
+  util.savejson('memes.json', memes);
+  if ('lastpost' in memes) {
+    var lastPostDate = new Date(memes['lastpost']);
+    if ('channel' in memes) {
+      var timeout = false;
+      if (!lastPostDate) {
+        timeout = 0;
+      }
+      else {
+        timeout = (6 * 60 * 60 * 1000) - (new Date() - lastPostDate);
+        if (timeout < 0) {
+          timeout = 0;
+        }
+      }
+      if (countNotPosted() < 10) {
+        setTimeout(() => {
+          getMemes(postMemeFetch);
+        }, timeout);
+      }
+      else {
+        setTimeout(postMemeFetch, timeout);
+      }
+    }
   }
 }
 
@@ -109,4 +98,40 @@ function countNotPosted() {
     }
   }
   return count;
+}
+
+function deleteOldMemes() {
+  const keys = Object.keys(memes.data);
+  for (var i=0;i<keys.length;i++) {
+    const dt = new Date(memes['data'][keys[i]]['created_utc'] * 1000);
+    if (((new Date() - dt) / 1000 / 60 / 60) >= 72) {
+      delete memes.data[keys[i]];
+    }
+  }
+}
+
+function sendMeme() {
+  const keys = Object.keys(memes.data);
+  var condition = false;
+  if ('lastpost' in memes) {
+    const lastPostDate = new Date(memes['lastpost']);
+    condition = ('channel' in memes && (new Date() - lastPostDate) / 1000 / 60 / 60 > 6);
+  }
+  else {
+    condition = ('channel' in memes);
+  }
+  if (condition) {
+    var key = false;
+    for (var i=0;i<keys.length;i++) {
+      if (!memes['data'][keys[i]]['posted']) {
+        key = keys[i];
+        break;
+      }
+    }
+    if (key) {
+      channel.send(memes['data'][keys[i]]['title'] + '\n' + memes['data'][keys[i]]['url']);
+      memes['data'][keys[i]]['posted'] = true;
+      memes['lastpost'] = new Date();
+    }
+  }
 }
