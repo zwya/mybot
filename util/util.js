6e5dbd07781faaf1b7c46c8c1d18ca9400015c07
Ahmed Hanie <zwya@ymail.com>
2020-02-08 13:07:43 +0200

    Bug fix

--- util/util.js
+++ util/util.js
@@ -1,3 +1,13 @@
+const fs = require('fs');
+
 module.exports.prefixify = (string, prefix) => {
   return string.replace(new RegExp('!', 'g'), prefix);
+}
+
+module.exports.savejson = (filename, json) => {
+  fs.writeFile(filename, JSON.stringify(json, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    }
+  });
 }
